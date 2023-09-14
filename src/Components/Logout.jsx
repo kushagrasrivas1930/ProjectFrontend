6f6c60f78b94d1ada420ba0ed068cc617cf947ba
unknown <null>
2023-09-14 06:35:56 +0000

    Profile Page completed

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-
+import '../css/logout.css';
 class Logout extends Component{
     render(){
         return(
-            
-        )
+            <div className='lgBtn'><span id='lgTxt'>Log out</span></div>
+        );
     }
 }
+
+export default Logout;
--- src/Components/profile.jsx
+++ src/Components/profile.jsx
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Options from "./Options";
 import Footer from "./footer";
+import Logout from './Logout';
 class Profile extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Options/>
-
+        <Options />
+        <Logout />
       </div>
     );
   }
