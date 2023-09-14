d39202a07c4f28daeadf722398cf8b895df3bcf1
unknown <null>
2023-09-14 07:02:50 +0000

    Profile page finale

--- src/Components/profile.jsx
+++ src/Components/profile.jsx
@@ -3,11 +3,14 @@ import Navbar from "./Navbar";
 import Options from "./Options";
 import Footer from "./footer";
 import Logout from './Logout';
+import '../css/profile.css';
 class Profile extends Component {
   render() {
     return (
       <div>
+        
         <Navbar />
+        <h1>Hello, Kushagra</h1>
         <Options />
         <Logout />
       </div>
